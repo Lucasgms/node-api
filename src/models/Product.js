6641a4c5b2a9d9fecb4dc08bef6a940993ddcb86
Lucas Gregory <lgregory12@hotmail.com>
2020-04-11 00:16:28 -0300

    [Chore] First commit

---
+++ src/models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const mongooPaginate = require('mongoose-paginate');
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+ProductSchema.plugin(mongooPaginate);
+
+mongoose.model('Product', ProductSchema);
